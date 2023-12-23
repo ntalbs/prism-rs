608f6ad1679371305ee23e578b463d83023fb072
ntalbs <ntalbsen@gmail.com>
2023-12-23 11:23:58 +0000

    Change input for checking more various cases

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::io;
 use scanner::Scanner;
 
 fn main() -> io::Result<()> {
-    let input = read_to_string("./src/main.rs")?;
+    let input = read_to_string("./src/scanner.rs")?;
     let mut scanner = Scanner::new(input.as_str());
     let result = scanner.scan();
     for l in result {
