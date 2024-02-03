1beb7eaffb26e115849cb3083bbd9b94cbf38eab
ntalbs <ntalbsen@gmail.com>
2024-02-03 16:57:44 +0000

    Add main

---
+++ src/bin/prism.rs
@@ -0,0 +1,14 @@
+use std::{env, fs, io};
+
+use prism_rs::Prism;
+
+fn main() -> io::Result<()> {
+    let files: Vec<String> = env::args().skip(1).collect();
+
+    for f in files {
+        println!(">>> {f}");
+        let source = fs::read_to_string(f)?;
+        Prism::new(&source).render_to_console_with_line_num();
+    }
+    Ok(())
+}
